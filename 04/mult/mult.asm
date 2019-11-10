3ebb764214a6bffd912778b2799a21d02beaf124
Tomonori HIRATA <hirata@sqlab.jp>
2019-11-10 22:20:47 +0900

    update 03 and 04

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,25 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R2
+  M = 0
+  @i
+  M = 1
+(LOOP)
+  @R1
+  D = M // D = R1
+  @i
+  D = D - M // D = D - i
+  @EXIT
+  D;JLT // D < 0 ならばEXITへ
+  @R0
+  D = M // D = R0
+  @R2
+  M = M + D // R2 = R2 + D
+  @i
+  M = M + 1 // i = i + 1
+  @LOOP
+  0;JMP
+(EXIT)
+  @EXIT
+  0;JMP   // Goto END (infinite loop)
